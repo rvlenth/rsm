d4c6c022a994c259eaf6def59f1bb9af75c73c70
rlenth <rlenth@7354427b-d295-4c4f-9a55-d8da99b2380c>
2016-11-19 02:42:55 +0000

    rsm -- expanded license, added copyright statements

--- rsm/R/lsmeans-support.R
+++ rsm/R/lsmeans-support.R
@@ -1,3 +1,22 @@
+##############################################################################
+#    Copyright (c) 2016 Russell V. Lenth                                     #
+#                                                                            #
+#    This file is part of the rsm package for R (*rsm*)                      #
+#                                                                            #
+#    *rsm* is free software: you can redistribute it and/or modify           #
+#    it under the terms of the GNU General Public License as published by    #
+#    the Free Software Foundation, either version 2 of the License, or       #
+#    (at your option) any later version.                                     #
+#                                                                            #
+#    *rsm* is distributed in the hope that it will be useful,                #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#    GNU General Public License for more details.                            #
+#                                                                            #
+#    A copy of the GNU General Public License is available at                #
+#    <https://www.r-project.org/Licenses/>                                   #
+##############################################################################
+
 # lsmeans support ...
 # We'll support a 'mode' argument with 3 possibilities:
 #    - "asis" - just passes throu like an lm model, no matter what
--- rsm/R/old-ccd-bbd.R
+++ rsm/R/old-ccd-bbd.R
@@ -1,3 +1,22 @@
+##############################################################################
+#    Copyright (c) 2008-2010, 2012-2014 Russell V. Lenth                     #
+#                                                                            #
+#    This file is part of the rsm package for R (*rsm*)                      #
+#                                                                            #
+#    *rsm* is free software: you can redistribute it and/or modify           #
+#    it under the terms of the GNU General Public License as published by    #
+#    the Free Software Foundation, either version 2 of the License, or       #
+#    (at your option) any later version.                                     #
+#                                                                            #
+#    *rsm* is distributed in the hope that it will be useful,                #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#    GNU General Public License for more details.                            #
+#                                                                            #
+#    A copy of the GNU General Public License is available at                #
+#    <https://www.r-project.org/Licenses/>                                   #
+##############################################################################
+
 ### This source file contains the versions of ccd and bbd from rsm version 1.41
 ### They are provided to allow for working around differences in rsm starting 
 ### with 2.00, and to allow one to exactly reproduce a design that was created
