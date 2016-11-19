7ae9fdb3321f891afb0739ba2ec25841b9bd975c
Russell V. Lenth <russell-lenth@uiowa.edu>
2016-11-19 02:42:55 +0000

    rsm -- expanded license, added copyright statements

--- R/rsm.R
+++ R/rsm.R
@@ -1,3 +1,22 @@
+##############################################################################
+#    Copyright (c) 2008-2010, 2012-2016 Russell V. Lenth                     #
+#                                                                            #
+#    This file is part of the rsm package for R (*rsm*)                      #
+#                                                                            #
+#    *rsm* is free software: you can redistribute it and/or modify           #
+#    it under the terms of the GNU General Public License as published by    #
+#    the Free Software Foundation, either version 2 of the License, or       #
+#    (at your option) any later version.                                     #
+#                                                                            #
+#    *rsm* is distributed in the hope that it will be useful,                #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#    GNU General Public License for more details.                            #
+#                                                                            #
+#    A copy of the GNU General Public License is available at                #
+#    <https://www.r-project.org/Licenses/>                                   #
+##############################################################################
+
 ### Functions to facilitate response-surface analysis
 
 # Nov 2012 mod: changed naming of effects...
