8ded5ba8dceb7a6da3ea919ebd5aae9849de1660
Russell V. Lenth <russell-lenth@uiowa.edu>
2017-10-23 15:05:05 -0500

    Fixed bug in model.data

--- R/contour-lm.R
+++ R/contour-lm.R
@@ -49,12 +49,17 @@ model.data = function (lmobj, lhs = FALSE) {
     trms = terms(lmobj)
     if (!lhs)
         trms = delete.response(trms)
-    form = reformulate(all.vars(trms))
-    fcall$formula = update(trms, form)
-    env = environment(trms)
-    if (is.null(env)) 
-        env = parent.frame()
-    eval(fcall, env, parent.frame())
+    vars = all.vars(trms)
+    if(!is.null(dat <- lmobj$data))
+        dat[, vars, drop = FALSE]
+    else {
+        form = reformulate(vars)
+        fcall$formula = update(trms, form)
+        env = environment(trms)
+        if (is.null(env)) 
+            env = parent.frame()
+        eval(fcall, env, parent.frame())
+    }
 }
 
 ### contour plot(s) for a lm
