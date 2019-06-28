54ddbd31157f48047b94ce2521034ba7b8ba48d5
Russell V. Lenth <russell-lenth@uiowa.edu>
2019-06-28 13:06:33 -0500

    New emmeans registration
    anda coupla minor bug fixes

--- R/zzz.R
+++ R/zzz.R
@@ -17,26 +17,8 @@
 #    <https://www.r-project.org/Licenses/>                                   #
 ##############################################################################
 
-### Dynamic registration of S3 methods
-# Code borrowed from hms pkg. I omitted some type checks etc. because
-# this is only for internal use and I solemnly promise to behave myself.
-register_s3_method = function(pkg, generic, class) {
-    fun = get(paste0(generic, ".", class), envir = parent.frame())
-    if (isNamespaceLoaded(pkg)) {
-        registerS3method(generic, class, fun, envir = asNamespace(pkg))
-    }
-    # Register hook in case package is later unloaded & reloaded
-    setHook(
-        packageEvent(pkg, "onLoad"),
-        function(...) {
-            registerS3method(generic, class, fun, envir = asNamespace(pkg))
-        }
-    )
-}
 
 .onLoad = function(libname, pkgname) {
-    if (requireNamespace("emmeans", quietly = TRUE)) {
-        register_s3_method("emmeans", "recover_data", "rsm")
-        register_s3_method("emmeans", "emm_basis", "rsm")
-    }
+    if (requireNamespace("emmeans", quietly = TRUE))
+        emmeans::.emm_register("rsm", pkgname)
 }
